a320258e6427c459daa1d48b5e7f6e774ac576a3
Your NameNilansha18 <you@example.comnilanshabisht@gmail.com>
2024-07-23 17:33:47 +0530

    update and delete tasks function

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/',views.addTask,name='addTask')
+    #Add Task functionality
+    path('addTask/',views.addTask,name='addTask'),
+
+    #  mark_as_done functionality
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name="mark_as_done"),
+    #  mark_as_done functionality
+    path('mark_as_undone/<int:pk>/',views.mark_as_undone, name="mark_as_undone"),
+
+    #  edit tasks functionality
+    path('edit_task/<int:pk>/',views.edit_task, name="edit_task"),
+
+    # delete tasks functionality
+    path('delete_task/<int:pk>/',views.delete_task, name="delete_task"),
 ]
